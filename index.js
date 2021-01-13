78473235974a9e29a106c830bae32d0919757d3b
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-12 21:23:41 -0500

    commit tree d31740d20acb0e6dddc3aa1de99cafe8952d0800

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  displayMessage(greet((timeString)))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  const parse_time = parseInt(time)
+  if (parse_time <= 11) {
+    return "Good Morning";
+  } else if (parse_time <= 16) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(t){
+  document.getElementById("greeting").innerText = t
+}
